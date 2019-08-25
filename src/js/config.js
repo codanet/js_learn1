948979716d535d13ca9ad97e4490379baf6f9d95
codanet <ttmts@ua.fm>
2019-08-25 00:46:16 -0700

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = '7b4347363bd1cf8cf9a7c1b0cbb27e42';
---
+++ src/js/index.js
@@ -0,0 +1,63 @@
+import Search from './models/Search';
+import Recipe from './models/Recipe';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
+
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+
+/**
+ * SEARCH CONTROLLER
+ */
+const controlSearch = async () => {
+    // 1) Get uquery from view
+    const query = searchView.getInput();
+
+    if (query) {
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+ 
+        // 4) SEarch for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
+
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+        
+    }
+});
+
+
+/**
+ * RECIPE CONTROLLER
+ */
+const r = new Recipe(47025);
+r.getRecipe();
+console.log(r);
